7f17e4da2589bb299f4993c9e95b62e5f3bf4e09
ernest-nnodu <ernest.nnodu@outlook.com>
2026-03-26 19:12:41 +0000

    Add Product repository

--- src/main/java/com/jackalcode/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/jackalcode/ecommerceapi/EcommerceApiApplication.java
@@ -3,6 +3,7 @@ package com.jackalcode.ecommerceapi;
 import com.jackalcode.ecommerceapi.entity.*;
 import com.jackalcode.ecommerceapi.repository.CategoryRepository;
 import com.jackalcode.ecommerceapi.repository.CustomerRepository;
+import com.jackalcode.ecommerceapi.repository.ProductRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,6 +21,10 @@ public class EcommerceApiApplication {
         var categoryRepository = context.getBean(CategoryRepository.class);
         Category category = categoryRepository.findById(1L).orElseThrow();
         System.out.println(category);
+
+        var productRepository = context.getBean(ProductRepository.class);
+        Product product = productRepository.findById(1L).orElseThrow();
+        System.out.println(product);
     }
 
 }
