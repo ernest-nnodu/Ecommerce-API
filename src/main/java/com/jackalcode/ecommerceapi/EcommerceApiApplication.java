0ccdf60a4aae886ba32fcce90e8e416f86594776
ernest-nnodu <ernest.nnodu@outlook.com>
2026-03-26 12:41:57 +0000

    Add customer entity

--- src/main/java/com/jackalcode/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/jackalcode/ecommerceapi/EcommerceApiApplication.java
@@ -1,5 +1,6 @@
 package com.jackalcode.ecommerceapi;
 
+import com.jackalcode.ecommerceapi.entity.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(EcommerceApiApplication.class, args);
+
     }
 
 }
