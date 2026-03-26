2b1a6597dbca4dcaab248fc3d9de585e301b81cd
ernest-nnodu <ernest.nnodu@outlook.com>
2026-03-26 18:47:59 +0000

    Add customer repository

--- src/main/java/com/jackalcode/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/jackalcode/ecommerceapi/EcommerceApiApplication.java
@@ -1,6 +1,7 @@
 package com.jackalcode.ecommerceapi;
 
 import com.jackalcode.ecommerceapi.entity.*;
+import com.jackalcode.ecommerceapi.repository.CustomerRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +12,11 @@ public class EcommerceApiApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(EcommerceApiApplication.class, args);
+        var context = SpringApplication.run(EcommerceApiApplication.class, args);
+
+        var userRepository = context.getBean(CustomerRepository.class);
+        var customer = userRepository.findById(1L).orElseThrow();
+        System.out.println(customer);
     }
 
 }
