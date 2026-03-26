dfced8ceff7d989d6b11a3907ad43db559102521
ernest-nnodu <ernest.nnodu@outlook.com>
2026-03-26 12:51:35 +0000

    Add category entity

--- src/main/java/com/jackalcode/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/jackalcode/ecommerceapi/EcommerceApiApplication.java
@@ -1,5 +1,6 @@
 package com.jackalcode.ecommerceapi;
 
+import com.jackalcode.ecommerceapi.entity.Category;
 import com.jackalcode.ecommerceapi.entity.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,17 @@ public class EcommerceApiApplication {
 
         SpringApplication.run(EcommerceApiApplication.class, args);
 
+        var customer = Customer.builder()
+                .id(1L)
+                .firstName("Jack")
+                .lastName("Alice")
+                .email("email")
+                .password("password")
+                .build();
+        System.out.println(customer);
+
+        var category = new Category(1, "Category 1");
+        System.out.println(category);
     }
 
 }
