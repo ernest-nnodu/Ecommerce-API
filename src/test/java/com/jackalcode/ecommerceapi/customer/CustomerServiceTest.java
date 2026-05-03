4a809089a358270fa7d474962013b488db0dbe4c
ernest-nnodu <ernest.nnodu@outlook.com>
2026-05-03 18:29:37 +0100

    Implement register user with valid credentials unit test

---
+++ src/test/java/com/jackalcode/ecommerceapi/customer/CustomerServiceTest.java
@@ -0,0 +1,95 @@
+package com.jackalcode.ecommerceapi.customer;
+
+import com.jackalcode.ecommerceapi.cart.Cart;
+import com.jackalcode.ecommerceapi.cart.CartRepository;
+import com.jackalcode.ecommerceapi.security.AuthenticationService;
+import com.jackalcode.ecommerceapi.security.Role;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerServiceTest {
+
+    @Mock
+    CustomerRepository customerRepository;
+
+    @Mock
+    CartRepository cartRepository;
+
+    @Mock
+    CustomerMapper customerMapper;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    @Mock
+    AuthenticationService authenticationService;
+
+    @InjectMocks
+    CustomerServiceImpl customerService;
+
+    @Test
+    @DisplayName("registerCustomer: given valid request, " +
+            "saves customer with hashed password and creates cart")
+    void registerCustomer_withValidCredentials_returnsCustomer() {
+
+        RegisterCustomerRequest request = new RegisterCustomerRequest(
+                "John", "Doe", "john.doe@mail.com", "Password123?");
+
+        Customer customer = createCustomerEntity();
+
+        CustomerResponse customerResponse = new CustomerResponse(
+                customer.getId(),
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail()
+        );
+
+        Cart cart = new Cart();
+        cart.setCustomer(customer);
+
+        when(customerRepository.existsByEmail(request.email())).thenReturn(false);
+        when(passwordEncoder.encode(request.password())).thenReturn("hashedPassword");
+        when(customerMapper.toCustomer(request)).thenReturn(customer);
+        when(customerRepository.save(any(Customer.class))).thenReturn(customer);
+        when(cartRepository.save(any(Cart.class))).thenReturn(cart);
+        when(customerMapper.toCustomerResponse(customer)).thenReturn(customerResponse);
+
+        CustomerResponse response = customerService.registerCustomer(request);
+
+        assertAll(
+                () -> Assertions.assertNotNull(response),
+                () -> Assertions.assertEquals("John", response.firstName()),
+                () -> Assertions.assertEquals("Doe", response.lastName()),
+                () -> Assertions.assertEquals("john.doe@mail.com", response.email())
+        );
+
+        verify(customerRepository).save(any(Customer.class));
+        verify(cartRepository).save(any(Cart.class));
+        verify(passwordEncoder).encode("Password123?");
+    }
+
+    private Customer createCustomerEntity() {
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("John");
+        customer.setLastName("Doe");
+        customer.setEmail("john.doe@mail.com");
+        customer.setPassword("Password123?");
+        customer.setRole(Role.USER);
+        return customer;
+
+    }
+}
+
